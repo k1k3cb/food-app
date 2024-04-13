4837ef959d3489d66bcf7036491a4868ab2197da
Enrique Comesaña <enriquecbar@gmail.com>
2024-04-13 23:41:22 +0200

    HeadlineCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import HeadlineCards from './components/HeadlineCards';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
@@ -6,6 +7,7 @@ const App = () => {
 		<div className=''>
 			<Navbar />
 			<Hero />
+			<HeadlineCards />
 		</div>
 	);
 };
